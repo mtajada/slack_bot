96654a4770a1a8b4e6416ae9b2caa333759a6142
mtajada <tajada2000@gmail.com>
2023-10-02 12:37:27 +0200

    Initial commit

---
+++ slack/slack_bot.py
@@ -0,0 +1,101 @@
+import os
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
+from slack_bolt.adapter.flask import SlackRequestHandler
+from slack_bolt import App
+from dotenv import find_dotenv, load_dotenv
+from flask import Flask, request
+from functions import draft_email
+
+# Load environment variables from .env file
+load_dotenv(find_dotenv())
+
+# Set Slack API credentials
+SLACK_BOT_TOKEN = os.environ["SLACK_BOT_TOKEN"]
+SLACK_SIGNING_SECRET = os.environ["SLACK_SIGNING_SECRET"]
+SLACK_BOT_USER_ID = os.environ["SLACK_BOT_USER_ID"]
+
+# Initialize the Slack app
+app = App(token=SLACK_BOT_TOKEN)
+
+# Initialize the Flask app
+flask_app = Flask(__name__)
+handler = SlackRequestHandler(app)
+
+
+def get_bot_user_id():
+    """
+    Get the bot user ID using the Slack API.
+    Returns:
+        str: The bot user ID.
+    """
+    try:
+        # Initialize the Slack client with your bot token
+        slack_client = WebClient(token=os.environ["SLACK_BOT_TOKEN"])
+        response = slack_client.auth_test()
+        return response["user_id"]
+    except SlackApiError as e:
+        print(f"Error: {e}")
+
+
+get_bot_user_id()
+
+
+#Task given to the assistant
+def my_function(text):
+    """
+    Custom function to process the text and return a response.
+    In this example, the function converts the input text to uppercase.
+
+    Args:
+        text (str): The input text to process.
+
+    Returns:
+        str: The processed text.
+    """
+    response = text.upper()
+    return response
+
+
+
+#Cuando se menciona @asistente se recoge el texto de a continuación que se usará como prompt.
+@app.event("app_mention")
+def handle_mentions(body, say):
+    """
+    Event listener for mentions in Slack.
+    When the bot is mentioned, this function processes the text and sends a response.
+
+    Args:
+        body (dict): The event data received from Slack.
+        say (callable): A function for sending a response to the channel.
+    """
+    text = body["event"]["text"]
+
+    mention = f"<@{SLACK_BOT_USER_ID}>"
+    text = text.replace(mention, "").strip()
+
+    response = draft_email(text)
+    say(response)
+
+
+
+@flask_app.route("/slack/events", methods=["POST"])
+def slack_events():
+    
+    data = request.json
+   
+    # Check if the request is a challenge request
+    if 'challenge' in data:
+        return jsonify({"challenge": data["challenge"]})
+
+    # Handle other events using the SlackRequestHandler
+    return handler.handle(request)
+
+
+
+    """
+    # Run the Flask app
+if __name__ == "__main__":
+    flask_app.run()
+    
+    """
